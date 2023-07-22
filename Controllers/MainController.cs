5360c21c3f3043eef92e4c34ec68d43a8d9f60ec
cabiste <ahmedjj354@gmail.com>
2023-07-22 15:44:51 +0100

    created the side bar

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
@@ -14,17 +15,66 @@ class MainController
     {
         _view = view;
         ConfigureMainHeader();
+        ConfigureSideBar();
     }
 
-    // subscribe to the OpenLink function on click
+    /// <summary>
+    /// subscribes the link buttons to the OpenLink function
+    /// </summary>
     private void ConfigureMainHeader()
     {
         _view.GithubButton.Click += OpenLink;
         _view.AboutButton.Click += OpenLink;
     }
 
-    // opens a link in the default browser
-    public void OpenLink(object? sender, RoutedEventArgs e)
+    /// <summary>
+    /// subscribes all the side bar buttons to the ChangeView function
+    /// </summary>
+    private void ConfigureSideBar()
+    {
+        // look through all the children of the sidebar
+        foreach (var child in _view.SideBar.Children)
+        {
+            // we only need buttons
+            if (child is Button)
+                // cast the variable to a button to get intelliSense
+                ((Button)child).Click += ChangeView;
+        }
+    }
+
+    /// <summary>
+    /// Changes the content of the main area
+    /// </summary>
+    private void ChangeView(object? sender, RoutedEventArgs e)
+    {
+        // get the tag of the button
+        string ViewName = ((Button)sender).Tag.ToString();
+
+        // we'll add the logic to switch views after we create them
+        switch (ViewName)
+        {
+            case "Home":
+                Console.WriteLine("Clicked on the 'home' button.");
+                break;
+
+            case "Counter":
+                Console.WriteLine("Clicked on the 'counter' button.");
+                break;
+
+            case "FetchData":
+                Console.WriteLine("Clicked on the 'fetch data' button.");
+                break;
+
+            default:
+                Console.WriteLine($"The view {ViewName} does not exist!");
+                break;
+        }
+    }
+
+    /// <summary>
+    /// opens a link in the default browser
+    /// </summary>
+    private void OpenLink(object? sender, RoutedEventArgs e)
     {
         // cast the sender to a button and get the Tag as a string
         string url = ((Button)sender).Tag.ToString();
